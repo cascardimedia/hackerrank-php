5a35fa09154fd10abd4a07725e6207d35fd47c48
Matt Cascardi <mattcascardi@gmail.com>
2021-03-27 21:03:10 +0000

    Final solution for Repeated String

--- interview-preparation/repeated-string.php
+++ interview-preparation/repeated-string.php
@@ -2,14 +2,25 @@
 // Repeated String
 
 // Complete the repeatedString function below.
+
 function repeatedString($s, $n) {
-    // $t = 1000000000000;
-    $data = count_chars($s, 1);
-    // $search  = str_repeat($s, 1000000000000);
-    $count = $data[ord('a')];
-    $length = strlen($s);
-    $ratio = $count / $length;
-    return number_format($n * $ratio, 0, '', '');
+    // Memory limit is 2097152
+    if ($n <= 2000000) {
+        $times = ceil( $n / strlen($s));
+        $string = substr(str_repeat($s, $times), 0, $n);
+        return substr_count($string, 'a');
+    } else {
+        // The entire string will not fit into memory so we must do
+        // this as a calculation and not a direct count operation
+        $strlen = strlen($s);
+        $r = $n % $strlen;
+
+        $c = substr_count($s, 'a');
+        $m = floor($n/$strlen);
+        $a = ($m * $c);
+        $a += substr_count(substr($s, 0, $r), 'a');
+        return $a;
+    }
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
